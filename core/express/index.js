b0257b4129e995eb8717cb84449fb9d557c97473
zhiwenbin <zhiwenbin200@163.com>
2022-09-11 07:10:13 +0800

    express update

--- core/express/index.js
+++ core/express/index.js
@@ -13,6 +13,31 @@ app.get('/', (req, res)=>{
     id: 2
   })
 })
+// // 字符串匹配：？前面的字符可有可无，返回值一样
+// app.get('/lists?', (req, res) => {
+//   res.send('/lists or /list is ok!')
+// })
+// // 字符串匹配：()+ 括号里面的内容可写可不写
+// app.get('/l(is)?t', (req, res) => {
+//   res.send('/list or liabcdefgst is ok!')
+// })
+// // 字符串匹配：+前面的字符可以是1～无穷个
+// app.get('/lists+', (req, res) => {
+//   res.send('/lists or /listssssss is ok!')
+// })
+// // 字符串匹配：*可以是任意字符
+// app.get('/li*st', (req, res) => {
+//   res.send('/lisdkjfsodfjst')
+// })
+// // 字符串匹配：正则表达式
+// app.get(/.*abc$/, (req, res) => {
+//   res.send('可以匹配正则表达式的路由')
+// })
+// // 类似 vue 的动态路由参数
+// app.get('/detail/:id', (req, res) => {
+//   console.log(req);
+//   res.send('动态参数')
+// })
 
 app.listen(3000, ()=> {
   console.log('express start');
---
+++ core/express/middle.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+// 路由后面的回调函数称为中间件，可以有多个中间件，通过调用 next() 进入下一个中间件
+// 写法1，不优雅
+app.get(
+  '/login',
+  (req, res, next) => {
+    console.log('进入第一个中间件');
+    if(true) {
+      // 进入下一个中间件
+      next()
+    }else {
+      res.send('验证失败')
+    }
+  },
+  (req, res) => {
+    res.send('验证通过，登录中...')
+  }
+)
+// 写法2，完美
+function cb1(req, res, next) {
+  console.log('执行中间件1');
+  next()
+}
+function cb2(req, res) {
+  console.log('执行中间件2');
+  res.send('loading...')
+}
+app.get('/home', [cb1, cb2])
+
+app.listen(3000, () => {
+  console.log('express start');
+})
