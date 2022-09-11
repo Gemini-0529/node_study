79fd53ea90099f71fce17f3e9b35ed135e8ae09b
zhiwenbin <zhiwenbin200@163.com>
2022-09-11 09:44:50 +0800

    接收请求参数

--- core/express/routerIndex.js
+++ core/express/routerIndex.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const router = require('./routerMiddle')
+// 使用express内置的解析post请求参数的中间件
+app.use(express.urlencoded({extended: false}))// uname=124&pwd=122
+app.use(express.json())// {"uname":123,"pwd":234}
 // 应用级别中间件
 app.use((req, res, next)=>{
   console.log('验证token');
--- core/express/routerMiddle.js
+++ core/express/routerMiddle.js
@@ -6,7 +6,16 @@ router.get('/home', (req, res) => {
   res.send('路由级别中间件   home')
 })
 router.get('/login', (req, res) => {
-  res.send('路由级别中间件   login')
+  console.log(req.query);
+  // res.send('路由级别中间件   login')
+  // 获取参数
+  res.send(req.query)
+})
+// post请求参数
+router.post('/loginpost', (req, res) => {
+  console.log(req.body);
+  // res.send('路由级别中间件   login')
+  res.send(req.body)
 })
 
 module.exports = router
